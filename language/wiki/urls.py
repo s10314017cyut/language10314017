ca5f0715372457642c75bfda8de404bcf6de320d
aa0983163178 <aa0983163178@gmail.com>
2015-11-20 16:37:52 +0800

    CH8 9

--- language/wiki/urls.py
+++ language/wiki/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     url(r'^about/$', views.about, name='about'),
     url(r'^$', views.wiki, name='wiki'),
     url(r'^category/(?P<categoryName>[\w\-]+)/$', views.category, name='category'),
+    url(r'^addCategory/$', views.addCategory, name='addCategory'), 
+    url(r'^addPage/(?P<categoryName>[\w\-]+)/$', views.addPage, name='addPage'),
+    url(r'^deleteCategory/(?P<categoryID>[0-9]+)/$', views.deleteCategory,name='deleteCategory'),
 ]
